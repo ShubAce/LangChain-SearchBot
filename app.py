31d9932a56ed3f8811ce53ccafc8f3b85d4edfa8
Shubham Suman <Shubhamsuman2005@gmail.com>
2025-06-24 23:50:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
+
+from langchain.agents import initialize_agent,AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_huggingface import HuggingFaceEmbeddings
+
+os.environ['HF_TOKEN'] = os.getenv("HF_TOKEN")
+groq_api = os.getenv("GROQ_API_KEY")
+
+arxiv_wrapper = ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=300)
+arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+wiki_wrapper = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=300)
+wiki = WikipediaQueryRun(api_wrapper=wiki_wrapper)
+
+search = DuckDuckGoSearchRun(name="Search")
+
+st.title("Langchain chat with search")
+
+st.sidebar.title("Settings")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role":"assistant","content":"Hi, I am a chatbot who can search the web. how can i help you?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"]) 
+
+if prompt:=st.chat_input(placeholder="what is machine learning?"):
+    st.session_state.messages.append({"role":"user","content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm=ChatGroq(api_key=groq_api , model="llama3-8b-8192",streaming=True)
+    tools = [wiki,arxiv,search]
+
+    search_agent = initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handling_parsing_errors=True)
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages,callbacks=[st_cb])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
